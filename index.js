5445b3fb8e542c985a5e1a6596d4f10f0dd3c5d9
Santiago Bálsamo <balsamosantiago3225@gmail.com>
2026-06-12 23:51:49 -0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*contador*/
 const FECHA_MUNDIAL = new Date("2026-07-19T16:00:00");
 
 setInterval(function() {
@@ -9,9 +10,22 @@ setInterval(function() {
     const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
     const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
-    document.getElementById("contador-dias").textContent = String(dias).padStart(2, "0");
-    document.getElementById("contador-horas").textContent = String(horas).padStart(2, "0");
-    document.getElementById("contador-minutos").textContent = String(minutos).padStart(2, "0");
-    document.getElementById("contador-segundos").textContent = String(segundos).padStart(2, "0");
+    document.getElementById("contador-dias").textContent = String(dias).padStart(2, 0);
+    document.getElementById("contador-horas").textContent = String(horas).padStart(2, 0);
+    document.getElementById("contador-minutos").textContent = String(minutos).padStart(2, 0);
+    document.getElementById("contador-segundos").textContent = String(segundos).padStart(2, 0);
 
 }, 1000);
+
+
+/*boto participar*/
+
+function sorteo() {
+    document.getElementById("cartel").classList.remove("oculto");
+}
+
+function cerrarCartel() {
+    document.getElementById("cartel").classList.add("oculto");
+}
+
+document.getElementById("btn-participar").addEventListener("click", sorteo);
